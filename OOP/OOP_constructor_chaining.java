46863718a17809b755c0d9970c4dd056a71ecd41
Sharvil Patil <sharvilpatil13@gmail.com>
2022-10-01 23:06:12 +0530

    constructor chaining

--- OOP/OOP_constructor_chaining.java
+++ OOP/OOP_constructor_chaining.java
@@ -1,17 +1,37 @@
+class Demo  
+{  
+ 
+    Demo()  
+    {  
+        this(80, 90);                                       // Step 4: will call parameterized constructor of base class
+        System.out.println("Base class default constructor called");        // Step 6: will print this
+    }  
+
+    Demo(int x, int y)  
+    {  
+        System.out.println("Base class parameterized constructor called");      // Step 5: print this
+    }  
+}  
+
+class Prototype extends Demo  
+{  
+    Prototype()                                                                             
+    {  
+        this("Java", "Python");                                    // Step 2: will call parameterized constructor of derived class
+        System.out.println("Derived class default constructor called");     // Step 8 will print this
+    }  
+
+    Prototype(String str1, String str2)  
+    {  
+        super();                                                             // Step 3: will call default constructor of base class
+        System.out.println("Derived class parameterized constructor called");        // Step 7: will print this
+    }  
+}  
+
 public class OOP_constructor_chaining 
-{
-    public OOP_constructor_chaining()
-    {
-        this("Java");                         // Step 2) Parameterized constructor is called using this() keyword
-        System.out.println("Default Constructor called");                   // Step 4) After executing this(), the remaining code will be executed.
-    }    
-
-    public OOP_constructor_chaining(String s)
-    {
-        System.out.println("Parameterized constructor is called");       // Step 3) code inside parameterized constructor will get executed
-    }
-
-    public static void main(String[] args) {
-        OOP_constructor_chaining obj = new OOP_constructor_chaining();           // Step 1) Default constructor will be called
-    }
-}
+{  
+    public static void main(String args[])   
+    {   
+        Prototype my_example = new Prototype();                 // Step 1: will call default constructor of derived class  
+    }   
+}  
